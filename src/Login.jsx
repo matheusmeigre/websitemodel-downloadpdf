344e055b83b87d99a5a03ea7e564c18ceb972eab
Matheus Meigre <matheusmeigre@hotmail.com>
2025-03-15 18:09:46 -0300

    add error and success messages

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,37 +4,45 @@ function Login() {
 
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (!name || !password) {
-      alert('Preencha todos os campos')
+      setErrorMessage('Preencha todos os campos')
       return
     }
 
     if (password.length < 6) {
-      alert('A senha precisa ter no mínimo 6 caracteres')
+      setErrorMessage('A senha precisa ter no mínimo 6 caracteres')
       return
     }
 
     if (name.length < 3) {
-      alert('O nome precisa ter no mínimo 3 caracteres')
+      setErrorMessage('O nome precisa ter no mínimo 3 caracteres')
       return
     }
 
     if (name === 'admin' && password === '123456') {
-      alert('Login realizado com sucesso')
+      setSuccessMessage('Login realizado com sucesso')
+      setErrorMessage('')
       handleNavigate()
       return
     } else {
-      alert('Usuário ou senha inválidos')
+      setErrorMessage('Usuário ou senha inválidos')
       return
     }
   }
 
   const handleNavigate = () => {
-    window.location.href = '/home'
+    localStorage.setItem('name', name)
+    localStorage.setItem('password', password)
+    setTimeout(() => {
+        window.location.href = '/home'
+    }, 1000)
   }
   console.log(name, password)
 
@@ -43,6 +51,8 @@ function Login() {
     <div id='container'>
       <div id='box-form'>
         <h1>Login</h1>
+        {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         <form action="">
           <input type="text" name="name" id="username" placeholder='Enter your name' onChange={(e) => setName(e.target.value)} />
           <input type="password" name="password" id="password" placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)} />
