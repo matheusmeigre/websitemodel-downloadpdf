ab950012a0404331ee17d63cf126d2613bdb9870
Matheus Meigre <matheusmeigre@hotmail.com>
2025-03-15 17:22:33 -0300

    add className link-fatura

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
             <h1>Home</h1>
             <p>Baixar faturas abaixo</p>
             <div id='box-faturas'>
-                <a href="/7356699.pdf" target='_blank'>Fatura 1</a>
-                <a href="/7356700.pdf" target='_blank'>Fatura 2</a>
-                <a href="/7356701.pdf" target='_blank'>Fatura 3</a>
-                <a href="/7356702.pdf" target='_blank'>Fatura 4</a>
+                <a href="/7356699.pdf" target='_blank' className='link-fatura'>Fatura 1</a>
+                <a href="/7356700.pdf" target='_blank' className='link-fatura'>Fatura 2</a>
+                <a href="/7356701.pdf" target='_blank' className='link-fatura'>Fatura 3</a>
+                <a href="/7356702.pdf" target='_blank' className='link-fatura'>Fatura 4</a>
             </div>
         </div>
     </div>
